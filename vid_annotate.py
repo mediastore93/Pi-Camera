9c897d5ba2990cb8d46459490f0b549562d6275b
mediastore93 <mediastore93@gmail.com>
2017-05-15 19:21:13 -0700

    Update vid_annotate.py

--- vid_annotate.py
+++ vid_annotate.py
@@ -7,7 +7,7 @@ camera.annotate_background = picamera.Color('black')
 camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 camera.start_recording('timestamped.h264')
 start = dt.datetime.now()
-while (dt.datetime.now() - start).seconds < 30:
+while (dt.datetime.now() - start).seconds < 300:
     camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     camera.wait_recording(0.2)
 camera.stop_recording()
