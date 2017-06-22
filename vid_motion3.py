9904372dba5a9f7c643635cdbb20cf77810eeb48
mediastore93 <nik@MacBook-Pro-7.local>
2017-06-22 14:02:12 -0700

    remove while loop

--- vid_motion3.py
+++ vid_motion3.py
@@ -72,46 +72,42 @@ def pir():
     Current_State  = 0
     Previous_State = 0
 
-
-
     print "Waiting for PIR to settle ..."
     # Loop until PIR output is 0
     while GPIO.input(GPIO_PIR)==1:
         Current_State  = 0
     print "  Ready"
-    # Loop until users quits with CTRL-C
-    while True :
-        # Read PIR state
-        Current_State = GPIO.input(GPIO_PIR)
-        timestamp = datetime.datetime.now().time()
-        start = datetime.time(6, 31)
-        end = datetime.time(19, 0)
-        midnight = datetime.time(23, 59)
-        if (Current_State==1 and Previous_State==0) and (start <= timestamp <= end):
-        # PIR is triggered
-            time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print "  Motion detected @ %s !" % time_now
-            video_rec()
-        elif (Current_State==1 and Previous_State==0) and (end < timestamp <= midnight):
-        # PIR is triggered
-            time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print "  Motion detected @ %s !" % time_now
-            still()
-        elif (Current_State==1 and Previous_State==0) and (timestamp < start):
-        # PIR is triggered
-            time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print "  Motion detected @ %s !" % time_now
-            still()
-        # Record previous state
-    	Previous_State=1
-        if (Current_State==0 and Previous_State==1):
-            # PIR has returned to ready state
-            stop_time=time.time()
-            print "  Ready "
-            Previous_State=0
 
-  #NMAP SCANNER:
+    # Read PIR state
+    Current_State = GPIO.input(GPIO_PIR)
+    timestamp = datetime.datetime.now().time()
+    start = datetime.time(6, 31)
+    end = datetime.time(19, 0)
+    midnight = datetime.time(23, 59)
+    if (Current_State==1 and Previous_State==0) and (start <= timestamp <= end):
+    # PIR is triggered
+        time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print "  Motion detected @ %s !" % time_now
+        video_rec()
+    elif (Current_State==1 and Previous_State==0) and (end < timestamp <= midnight):
+    # PIR is triggered
+        time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print "  Motion detected @ %s !" % time_now
+        still()
+    elif (Current_State==1 and Previous_State==0) and (timestamp < start):
+    # PIR is triggered
+        time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print "  Motion detected @ %s !" % time_now
+        still()
+    # Record previous state
+    Previous_State=1
+    if (Current_State==0 and Previous_State==1):
+        # PIR has returned to ready state
+        stop_time=time.time()
+        print "  Ready "
+        Previous_State=0
 
+#NMAP SCANNER:
 def sweep():
         count=0
         print('----------------------------------------------------')
@@ -123,7 +119,7 @@ def sweep():
                 print('State : %s' % nm[host].state())
                 if nm[host].state()=='up':
                         count+=1
-                        print('OG is in, counted up by 1')
+                        #print('OG is in, counted up by 1')
         if count > 0:
                 print('Count is %s, OG is in. Scanning again in 2 minutes' % count)
                 time.sleep(120)
