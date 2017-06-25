f3757eb7a9ccb8859613af2c1e30f67ae5bbd841
mediastore93 <mediastore93@gmail.com>
2017-06-25 16:24:21 -0700

    loop

--- vid_motion3.py
+++ vid_motion3.py
@@ -65,21 +65,21 @@ def still():
 #PIR ---------------------------------------------------
 def pir():
 
+    print("PIR has been activated (CTRL-C to exit)")
+    # Set pin as input
+    GPIO.setup(GPIO_PIR,GPIO.IN)
+
+    Current_State  = 0
+    Previous_State = 0
+
+    print "Waiting for PIR to settle ..."
+    # Loop until PIR output is 0
+    while GPIO.input(GPIO_PIR)==1:
+        Current_State  = 0
+    print "  Ready"
+
     t_end = time.time() + 60
     while time.time() < t_end:
-
-        print("PIR has been activated (CTRL-C to exit)")
-        # Set pin as input
-        GPIO.setup(GPIO_PIR,GPIO.IN)
-
-        Current_State  = 0
-        Previous_State = 0
-
-        print "Waiting for PIR to settle ..."
-        # Loop until PIR output is 0
-        while GPIO.input(GPIO_PIR)==1:
-            Current_State  = 0
-        print "  Ready"
 
         # Read PIR state
         Current_State = GPIO.input(GPIO_PIR)
@@ -104,10 +104,10 @@ def pir():
             still()
         # Record previous state
         Previous_State=1
-        else (Current_State==0 and Previous_State==1):
+        elif (Current_State==0 and Previous_State==1):
             # PIR has returned to ready state
             stop_time=time.time()
-            print "  Ready "
+            print "  Ready to start again "
             Previous_State=0
 
 #NMAP SCANNER:
