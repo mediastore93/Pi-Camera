f1702bdad6cc25e740c05cb2f103b50f385ee74d
mediastore93 <nik@MacBook-Pro-7.local>
2017-08-05 13:22:45 -0700

    added time of rec

--- vid_motion_simple.py
+++ vid_motion_simple.py
@@ -4,17 +4,20 @@ from datetime import datetime
 
 camera = PiCamera()
 pir = MotionSensor(17)
-#camera.rotation = 180
+
+camera.rotation = 180
 camera.framerate = 30
-camera.resolution = (480, 270)
-save_path = '/home/pi/vid/'
+camera.resolution = (1296, 972)
+save_dir = "/home/pi/vid/"
 
 while True:
     pir.wait_for_motion()
+    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
     camera.start_recording(filename)
-    print("Motion")
-    camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  
+    print("Motion detected, started recording @ %s" % (time_now))
+    camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     pir.wait_for_no_motion()
-    print("Motion stopped")
     camera.stop_recording()
+    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print("Stopped recording @ %s" % (time_now))
