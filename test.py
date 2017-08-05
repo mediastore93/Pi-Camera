c3916819f1f2432a6622cd36c06b0287cfc78a99
mediastore93 <mediastore93@gmail.com>
2017-08-05 16:26:08 -0700

    Update test.py

--- test.py
+++ test.py
@@ -12,12 +12,12 @@ save_dir = "/home/pi/vid/"
 
 while True:
     pir.wait_for_motion()
-    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
     camera.start_recording(filename)
     print("Motion detected, started recording @ %s" % (time_now))
     camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     pir.wait_for_no_motion()
     camera.stop_recording()
-    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print("Stopped recording @ %s" % (time_now))
