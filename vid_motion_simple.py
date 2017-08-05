a6a7a12fcb669277109ae6873f66c7c9758f84b3
mediastore93 <mediastore93@gmail.com>
2017-08-05 14:52:08 -0700

    Update vid_motion_simple.py

--- vid_motion_simple.py
+++ vid_motion_simple.py
@@ -1,6 +1,7 @@
 from gpiozero import MotionSensor
 from picamera import PiCamera
-import datetime
+from datetime import datetime
+
 
 camera = PiCamera()
 pir = MotionSensor(17)
@@ -11,15 +12,16 @@ camera.resolution = (1296, 972)
 save_dir = "/home/pi/vid/"
 
 while True:
-    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print("PIR started @ %s" % (time_now))
     pir.wait_for_motion()
-    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
     camera.start_recording(filename)
     print("Motion detected, started recording @ %s" % (time_now))
     camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     pir.wait_for_no_motion()
     camera.stop_recording()
-    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print("Stopped recording @ %s" % (time_now))
+
