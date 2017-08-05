aa80ee78f3132bd804e75a488e92bacf1df47b10
mediastore93 <nik@MacBook-Pro-7.local>
2017-08-05 15:14:58 -0700

    dir

--- vid_motion_simple.py
+++ vid_motion_simple.py
@@ -17,11 +17,10 @@ while True:
     pir.wait_for_motion()
     time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
-    camera.start_recording(filename)
+    camera.start_recording(save_dir, filename)
     print("Motion detected, started recording @ %s" % (time_now))
     camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     pir.wait_for_no_motion()
     camera.stop_recording()
     time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print("Stopped recording @ %s" % (time_now))
-
+    #print("Stopped recording @ %s" % (time_now))
